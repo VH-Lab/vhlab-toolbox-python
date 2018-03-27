b6e6b223fef54277c9e1761bbddfa88cf2c77ce3
yanglite1211 <yanglite1211@gmail.com>
2018-03-27 16:53:11 -0400

    Updated Mar 27th

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 25 18:39:58 2018
+
+@author: yanglite
+"""
+
--- fileio/string2filestring.py
+++ fileio/string2filestring.py
@@ -5,16 +5,21 @@ Created on Sun Mar 18 13:34:00 2018
 
 @author: yanglite
 """
-import string
+import re
 
 def string2filestring(s):
     # STRING2FILESTRING - edit a string so it its suitable for use as part of a filename (remove whitespace)
     # Example:
     # mystr = 'This is a variable name: 1234.';
     # string2filestring(mystr)  % returns 'This_is_a_variable_name__1234_'
+    """
+    # This is another way to do it
     ranges=string.ascii_lowercase+string.ascii_uppercase+string.digits
     s=list(s)
     for i in range(len(s)):
         if s[i] not in ranges:
             s[i]='_'
     return "".join(s)
+    """
+    return re.sub('\W', '_', s)
+
